37506d30c8c57764ad01cfcc69a0cdaab9e892e6
Daniel Alvarado <Daniel Alvarado daniel310787@hotmail.com>
2021-03-02 16:35:48 +0100

    adding all exercises

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -1,9 +1,16 @@
-function loadBooks(array) {
-
+var books = [
+    { title: 'The Design of EveryDay Things',  author: 'Don Norman', alreadyRead: false }, 
+    { title: 'The power of now', author: 'Ekhart Tolle', alreadyRead: true }, 
+    { title: 'The Three Body Problem', author: 'Liu Cixin', alreadyRead: true }
+  ];
+function loadBooks() {
+    for(let i = 0; i < books.length; i++){
+        let book = document.createElement("p");
+        let author = document.createTextNode(books[i].title + " by " + books[i].author)
+        book.appendChild(author);
+        document.body.appendChild(book);
+    }
 }
-
-// Paste here your array
-
-
 // This function will execute when the window loads
-window.onload = loadBooks(array);
+window.onload = loadBooks();
+
--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,9 +1,20 @@
+// # Calculate the sphere
+
+// Write a JavaScript program to calculate the volume of a sphere.
+
+// ## Extra
+
+// Show also time and date of now.
+
+
 function volume_sphere() {
 
-  // This is how you calculate the volume of the sphere
-  radius = Math.abs(Number(radius));
+  var volume;
+  var radius = document.getElementById("radius").value;
+  radius = Math.abs(radius);
   volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
   volume = volume.toFixed(4);
-
+  document.getElementById("volume").value = volume;  
+  var d = new Date();
+  document.getElementById("dttm").innerHTML = d;
 }
-
